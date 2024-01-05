4988bd46f2c16eb2d6a92b53e14a23e1b37bd170
Reda Salmi <reda.salmi.elt@gmail.com>
2024-01-05 18:11:30 +0100

    removed config files

--- config/zsh/.zsh/aliases.zsh
+++
@@ -1,3 +0,0 @@
-# aliases
-alias reflector-update="sudo reflector --country France,Germany --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
-alias rm-orphans="sudo pacman -Qtdq | sudo pacman -Rns -"
--- config/zsh/.zsh/plugins.zsh
+++
@@ -1,3 +0,0 @@
-# plugins
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
